a50be650cda1833e5b4514060d06d5ac5cb0b62e
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-02-28 20:00:02 +0100

    ajout des exercices partie 2

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -1,12 +1,23 @@
 // 02-maths/04-sort-numbers/script.js - 2.4: Sorting the numbers
 
-(() => {
-    document.getElementById("run").addEventListener("click", () => {
-        const x = new Array(2, 4, 14, 10, 90, 23, 16);
-        x.sort((a, b) => a - b);
-        alert(x)
-    });
-})();
+//(() => {
+//    document.getElementById("run").addEventListener("click", () => {
+//        const x = new Array(2, 4, 14, 10, 90, 23, 16);
+//        x.sort((a, b) => a - b);
+//        alert(x)
+//    });
+//})();
 
 //le programme fonctionne correctement 
 //TODO : faire un programme similaire avec méthode getElementByID
+
+(() => {
+    document.getElementById("run").addEventListener("click", () => {
+       let x = document.getElementById("numbers").value;
+       let arr = [x] ; 
+       arr = x.split()
+       let arr_sort = arr.sort((a, b) => a - b);
+       alert(arr_sort)
+   });
+})();
+
--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -2,17 +2,15 @@
 // 02-maths/06-fizzbuzz/script.js - 2.6: fizzbuzz
 
 (() => {
-    const readlineSync = require("readline-sync");
-
-    let x = new Number(readlineSync.question("Give me a number : ")) ;
-
-    if (x % 3 == 0) {
+    for (let x = 1; x<101; x++) {
+    if (x % 3 === 0) {
         console.log("fizz")
-    } else if (x % 5 == 0) {
+    } else if (x % 5 === 0) {
         console.log("buzz")
-    } else if (x % 3 == 0 && x % 3 == 0) {
+    } else if (x % 3 === 0 && x % 3 === 0) {
         console.log("fizzbuzz")
     } else {
-        console.log(x)
+       console.log(x)
+    }
     }
 })();
